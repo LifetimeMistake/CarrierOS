0ab3167b3eed00afb421985574cb9d1651644e7b
LifetimeMistake <57274704+LifetimeMistake@users.noreply.github.com>
2024-12-25 22:39:39 +0100

    Add logging subsystem

---
+++ components/logging.lua
@@ -0,0 +1,73 @@
+local RingBuffer = require("libs.ringBuffer")
+
+local LogLevel = {
+    DEBUG = 0,
+    INFO = 1,
+    WARN = 2,
+    ERROR = 3
+}
+
+LogLevel.tostring = function(level)
+    if level == LogLevel.DEBUG then
+        return "DEBUG"
+    end
+    if level == LogLevel.INFO then
+        return "INFO"
+    end
+    if level == LogLevel.WARN then
+        return "WARN"
+    end
+    if level == LogLevel.ERROR then
+        return "ERROR"
+    end
+
+    error("Unknown log level")
+end
+
+local Logger = {}
+Logger.__index = Logger
+
+function Logger.new(capacity)
+    return setmetatable({
+        ringBuffer = RingBuffer.new(capacity or 100),
+        logHook = nil
+    }, Logger)
+end
+
+function Logger:log(level, message)
+    self.ringBuffer:push(level, message)
+    if self.logHook then
+        self.logHook(level, message)
+    end
+end
+
+function Logger:debug(message)
+    self:log(LogLevel.DEBUG, message)
+end
+
+function Logger:info(message)
+    self:log(LogLevel.INFO, message)
+end
+
+function Logger:warn(message)
+    self:log(LogLevel.WARN, message)
+end
+
+function Logger:error(message)
+    self:log(LogLevel.ERROR, message)
+end
+
+function Logger:setHook(hook)
+    self.logHook = hook
+end
+
+function Logger:iter()
+    return self.ringBuffer:iter()
+end
+
+local logging = {
+    Logger = Logger,
+    LogLevel = LogLevel
+}
+
+return logging
---
+++ libs/ringBuffer.lua
@@ -0,0 +1,48 @@
+local RingBuffer = {}
+RingBuffer.__index = RingBuffer
+
+--- Creates a new ring buffer with the specified maximum size
+-- @param max_size The maximum number of messages the ring buffer can hold
+function RingBuffer.new(max_size)
+    return setmetatable({
+        buffer = {},
+        max_size = max_size,
+        start = 1,
+        size = 0
+    }, RingBuffer)
+end
+
+--- Push a new log message to the ring buffer
+-- @param self The ring buffer instance
+-- @param level The log level ("debug", "info", "warn", "error")
+-- @param message The log message text
+function RingBuffer:push(level, message)
+    local timestamp = os.clock()
+    local entry = { level = level, timestamp = timestamp, message = message }
+
+    -- Determine the position to write in the ring buffer
+    local pos = (self.start + self.size - 1) % self.max_size + 1
+
+    self.buffer[pos] = entry
+
+    if self.size < self.max_size then
+        self.size = self.size + 1
+    else
+        -- Advance the start index when overwriting oldest entry
+        self.start = self.start % self.max_size + 1
+    end
+end
+
+--- Iterate over the messages in the ring buffer in order
+-- @param self The ring buffer instance
+-- @return Iterator function returning each log entry
+function RingBuffer:iter()
+    local index = 0
+    return function()
+        if index < self.size then
+            local pos = (self.start + index - 1) % self.max_size + 1
+            index = index + 1
+            return self.buffer[pos]
+        end
+    end
+end
