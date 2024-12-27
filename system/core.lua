b1c6b0711ce0adb85ac3df3fbf16ea179c19be12
LifetimeMistake <57274704+LifetimeMistake@users.noreply.github.com>
2024-12-27 22:19:49 +0100

    Protect kernel exports

--- system/core.lua
+++ system/core.lua
@@ -2,17 +2,53 @@ if kernel then
     error("Kernel object already registered", 2)
 end
 
-_G.kernel = {}
-kernel.subsystems = {}
+-- Protect kernel and exports
+local kernel = {
+    subsystems = {},
+    exports = {}
+}
+
+local function createKernelOverlay()
+    local ko = kernel
+    local G = _G
+    local rawget = rawget
+
+    local overlay = setmetatable({}, {
+        __index = function (_, k)
+            return ko.exports[k] or rawget(G, k)
+        end,
+        __newindex = function (_, k, v)
+            if ko.exports[k] then
+                error("Kernel export collision: " .. k, 2)
+            end
+
+            ko.exports[k] = v
+        end
+    })
+
+    setmetatable(_G, {
+        __index = overlay,
+        __newindex = function (_, k, v)
+            overlay[k] = v
+        end
+    })
+
+    return overlay
+end
+
+local kOverlay = createKernelOverlay()
+setfenv(1, kOverlay)
+
+_G.kernel = kernel
 
 -- Set up require for the kernel space
 local make_package = dofile("/rom/modules/main/cc/require.lua").make
 local require
 do
-    local kernelEnv = { kernel = kernel }
-    kernelEnv.require, kernelEnv.package = make_package(kernelEnv, "/")
-    kernelEnv = setmetatable(kernelEnv, { __index = _ENV })
-    require = kernelEnv.require
+    local requireEnv = { kernel = kernel }
+    requireEnv.require, requireEnv.package = make_package(requireEnv, "/")
+    requireEnv = setmetatable(requireEnv, { __index = _ENV })
+    require = requireEnv.require
 end
 local expect = require("cc.expect").expect
 local exception = require "cc.internal.exception"
@@ -39,6 +75,10 @@ function kernel.listSubsystems()
     return t
 end
 
+function kernel.isKernelExport(name)
+    return kernel.exports[name] ~= nil
+end
+
 local function requireSubsystem(name)
     local ko = require(name)
     if not ko or not ko.is_kernel_subsystem then
@@ -51,12 +91,13 @@ end
 local function boot()
     -- Load kernel subsystems
     requireSubsystem("system.logging")
+    requireSubsystem("system.process")
 
     printk("Kernel load complete")
     printk("Loaded " .. #kernel.listSubsystems() .. " subsystems")
 
     printk("Starting init process")
-    os.sleep(10)
+    os.sleep(60)
 end
 
 boot()
