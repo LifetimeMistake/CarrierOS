e608774bc69303b76639c57711427b3f97bc128c
LifetimeMistake <57274704+LifetimeMistake@users.noreply.github.com>
2024-12-27 20:30:41 +0100

    Define structure for kernel objects

---
+++ system/ko.lua
@@ -0,0 +1,31 @@
+local ko = {}
+
+function ko.new()
+    return {
+        name = nil,
+        is_kernel_subsystem = false,
+        is_kernel_module = false,
+        exports = nil,
+        load_func = nil,
+        unload_func = nil
+    }
+end
+
+function ko.subsystem(name, exports)
+    local subsystem = ko.new()
+    subsystem.name = name
+    subsystem.is_kernel_subsystem = true
+    subsystem.exports = exports
+
+    return subsystem
+end
+
+function ko.module(name, load_func, unload_func)
+    local module = ko.new()
+    module.name = name
+    module.is_kernel_module = true
+    module.load_func = load_func
+    module.unload_func = unload_func
+end
+
+return ko
