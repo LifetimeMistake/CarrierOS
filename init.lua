9be97e7492ec90980c49859ef82faf40628bedf7
LifetimeMistake <57274704+LifetimeMistake@users.noreply.github.com>
2024-12-28 15:24:07 +0100

    Add init program & config

---
+++ init.lua
@@ -0,0 +1,25 @@
+local sShell
+if term.isColour() and settings.get("bios.use_multishell") then
+    sShell = "rom/programs/advanced/multishell.lua"
+else
+    sShell = "rom/programs/shell.lua"
+end
+
+if not process and not services then 
+    os.run({}, sShell)
+    return
+end
+
+services.register(
+    "carrieros_server", 
+    "/carrieros.lua",
+    {
+        restartPolicy = "on-failure:5",
+        loggerCapacity = 100
+    }
+)
+
+services.start("carrieros_server")
+term.clear()
+term.setCursorPos(0, 0)
+process.runFile(sShell, nil, false)
