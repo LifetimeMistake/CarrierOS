d402e26b160077d54af8060ff6c17e4f52ae4ed8
LifetimeMistake <57274704+LifetimeMistake@users.noreply.github.com>
2024-12-28 16:22:15 +0100

    Add common consts

---
+++ components/consts.lua
@@ -0,0 +1,18 @@
+return {
+    THRUST_PROFILES_DIR = "/data/profiles",
+    THRUSTER_CONFIG_PATH = "/data/thruster_config.json",
+    STATE_PERSIST_PATH = "/data/persist.json",
+    SYSTEM_CONFIG_PATH = "/data/system_config.json",
+    THRUSTER_NAMES = {
+        "main_front_left",
+        "main_front_right",
+        "main_back_left",
+        "main_back_right",
+        "thrust_left",
+        "thrust_right",
+        "corr_front_left",
+        "corr_front_right",
+        "corr_back_left",
+        "corr_back_right"
+    }
+}
